8e80c2deba1df4d01ca50988e1b7e3003eb35bcc
zseaborn63 <zseaborn63@gmail.com>
2015-11-05 14:40:29 -0500

    Conflicts resolved

--- Overflow/Q_and_A/views.py
+++ Overflow/Q_and_A/views.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
+from Q_and_A.models import Question
 
 
 class WelcomeView(TemplateView):
     template_name = 'welcome.html'
 
-
 class UserCreate(CreateView):
     model = User
     success_url = "/accounts/login"
     form_class = UserCreationForm
+
+class QuestionListView(ListView):
+    model = Question
+
