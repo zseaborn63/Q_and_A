17756e1757c440ad1e0098a42af96f911903f710
andbar <barringera@gmail.com>
2015-11-05 14:09:01 -0500

    Registered models

--- Overflow/Q_and_A/admin.py
+++ Overflow/Q_and_A/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from Q_and_A.models import Profile, Question, Answer, Tag, Vote
+
+admin.site.register(Profile)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
+admin.site.register(Vote)
