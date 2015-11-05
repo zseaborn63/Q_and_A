bb5d55c70ec500e15c839e7b46b15b3bd5cf0031
andbar <barringera@gmail.com>
2015-11-05 13:48:05 -0500

    Added Profile model and migration

--- Overflow/Q_and_A/models.py
+++ Overflow/Q_and_A/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(Model):
+    user = models.OneToOneField(User)
+    email = models.EmailField(blank=True)
+
+
+@receiver(post_save, sender=User)
+def user_post_save(sender, **kwargs):
+    instance = kwargs.get("instance")
+    created = kwargs.get("created")
+    if created:
+        Profile.objects.create(user=instance)
